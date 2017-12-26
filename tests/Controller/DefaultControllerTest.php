270e8cd19d950ad6f406088b15631e93b7a21078
Romain Norberg <romainnorberg@gmail.com>
2017-12-26 09:31:19 +0100

    🎨  add responseListenerTest

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,10 +2,27 @@
 
 namespace App\Tests;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @var \Symfony\Component\HttpKernel\Client
+     */
+    protected $client;
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function setUp()
+    {
+        $this->client = $client = self::createClient();
+        $this->container = $this->client->getContainer();
+    }
+
     /**
      * @dataProvider urlProvider
      *
@@ -13,10 +30,9 @@ class DefaultControllerTest extends WebTestCase
      */
     public function testPageIsSuccessful($url)
     {
-        $client = self::createClient();
-        $client->request('GET', $url);
+        $this->client->request('GET', $url);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
     public function urlProvider()
---
+++ tests/Listener/ResponseListenerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Tests;
+
+use App\EventListener\ResponseListener;
+use App\Kernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class ResponseListenerTest extends TestCase
+{
+    private $dispatcher;
+
+    private $kernel;
+
+    protected function setUp()
+    {
+        $this->kernel = new Kernel('prod', true); # simulate production
+        $this->dispatcher = new EventDispatcher();
+        $listener = new ResponseListener($this->kernel);
+        $this->dispatcher->addListener(KernelEvents::RESPONSE, [$listener, 'onKernelResponse']);
+
+    }
+
+    protected function tearDown()
+    {
+        $this->dispatcher = null;
+        $this->kernel = null;
+    }
+
+    /**
+     * @dataProvider urlProvider
+     *
+     * @covers       ResponseListener::onKernelResponse()
+     *
+     * @param string $value
+     * @param        $expected
+     */
+    public function testResponseEncoding($value, $expected)
+    {
+        $listener = new ResponseListener($this->kernel);
+        $this->dispatcher->addListener(KernelEvents::RESPONSE, [$listener, 'onKernelResponse'], 1);
+
+        $response = new Response('foo');
+        $server = [
+            'HTTP_ACCEPT_ENCODING' => $value,
+        ];
+        $request = Request::create('/', $method = 'GET', $parameters = [], $cookies = [], $files = [], $server, $content = null);
+
+        $event = new FilterResponseEvent($this->kernel, $request, HttpKernelInterface::MASTER_REQUEST, $response);
+        $this->dispatcher->dispatch(KernelEvents::RESPONSE, $event);
+
+        $this->assertEquals($expected, $response->headers->get('Content-encoding'));
+    }
+
+    public function urlProvider()
+    {
+        yield 'empty' => [
+            null,
+            null,
+        ];
+
+        yield 'gzip' => [
+            'gzip, deflate, br',
+            'gzip',
+        ];
+
+        yield 'deflate' => [
+            'deflate',
+            'deflate',
+        ];
+    }
+}
